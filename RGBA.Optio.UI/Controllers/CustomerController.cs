f819b45d761505612bd9b92d125fa8738e8fa4c1
RAISA <RAISA@DESKTOP-Q5BH0M9>
2024-04-06 16:00:49 +0400

    add:controller in Ui layer

---
+++ RGBA.Optio.UI/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RGBA.Optio.UI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetDemo()
+        {
+            await Task.Delay(200);
+            return Ok("...");
+        }
+    }
+
+}

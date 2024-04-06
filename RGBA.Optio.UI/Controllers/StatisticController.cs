70c55e7a9ab2391e8288d5368dbe386f05793c42
Guga Apkhazava <Guga Apkhazava@DESKTOP-EEG6HFA>
2024-04-06 16:08:42 +0400

    ADD: Ui Layer , Controller ,Named statistic

---
+++ RGBA.Optio.UI/Controllers/StatisticController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RGBA.Optio.UI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetDemo()
+        {
+            return Ok("Hello Word");
+        }
+    }
+}
